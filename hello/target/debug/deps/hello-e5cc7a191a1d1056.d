816a9124fbed0cf3c88da382a87f8b4697035690
hrantt <me@hrant.am>
2022-07-03 22:42:49 +0400

    cargo hello world

---
+++ hello/target/debug/deps/hello-e5cc7a191a1d1056.d
@@ -0,0 +1,5 @@
+/home/guts/scripts/rust_projects/rust_cli/hello/target/debug/deps/hello-e5cc7a191a1d1056: src/main.rs
+
+/home/guts/scripts/rust_projects/rust_cli/hello/target/debug/deps/hello-e5cc7a191a1d1056.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/guts/scripts/rust_projects/rust_cli/hello/target/debug/hello: /home/guts/scripts/rust_projects/rust_cli/hello/src/main.rs
